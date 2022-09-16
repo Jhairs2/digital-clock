d76f194d7d01ee6ea945c823e62a954b625b9964
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-16 00:56:50 -0400

    Forgot to commit again lol changes to js, html, and css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 clock = document.querySelector(".time");
-
+amPm = document.querySelector(".P-A");
 
 setInterval(function() {
    setClock();
@@ -11,8 +11,10 @@ setInterval(function() {
 
 function setClock() {
     let date = new Date();
+    let noonOrMorn = date.getHours() > 12 ? "PM" : "AM";
     time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }).replace(/AM|PM/,'') ;
     clock.textContent = time;
+    amPm.textContent = noonOrMorn;
 }
 
 
